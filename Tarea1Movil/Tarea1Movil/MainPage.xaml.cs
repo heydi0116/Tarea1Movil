9cbf0a4e94749bb9476e5bac32f73d8d38407384
heydi0116 <heydilishernandez@gmail.com>
2022-02-13 21:19:22 -0600

    first commit

---
+++ Tarea1Movil/Tarea1Movil/MainPage.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Tarea1Movil
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        async private void btnSuma_Clicked(object sender, EventArgs e)
+        {
+            var procs = new Models.resultados
+            {
+                resul = Convert.ToInt32(this.txtN1.Text) + Convert.ToInt32(this.txtN2.Text)
+
+            };
+            var pagina = new Page1();
+            pagina.BindingContext = procs;
+            await Navigation.PushAsync(pagina);
+
+        }
+
+        async private void btnResta_Clicked(object sender, EventArgs e)
+        {
+            var procs = new Models.resultados
+            {
+               resul = Convert.ToInt32(this.txtN1.Text) - Convert.ToInt32(this.txtN2.Text)
+
+            };
+            var pagina = new Page1();
+            pagina.BindingContext = procs;
+            await Navigation.PushAsync(pagina);
+
+        }
+
+        async private void btnDivision_Clicked(object sender, EventArgs e)
+        {
+            var procs = new Models.resultados
+            {
+                resul = Convert.ToInt32(this.txtN1.Text) / Convert.ToInt32(this.txtN2.Text)
+
+            };
+            var pagina = new Page1();
+            pagina.BindingContext = procs;
+            await Navigation.PushAsync(pagina);
+
+        }
+
+        async private void btnMultiplicacion_Clicked(object sender, EventArgs e)
+        {
+            var procs = new Models.resultados
+            {
+                resul = Convert.ToInt32(this.txtN1.Text) * Convert.ToInt32(this.txtN2.Text)
+
+            };
+            var pagina = new Page1();
+            pagina.BindingContext = procs;
+            await Navigation.PushAsync(pagina);
+
+        }
+    }
+}
---
+++ Tarea1Movil/Tarea1Movil/Models/resultados.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tarea1Movil.Models
+{
+    public class resultados
+    {
+        public int resul { get; set; }
+    }
+}
